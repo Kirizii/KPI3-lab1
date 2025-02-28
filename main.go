7a65b4bde551a182c82886b7e59ba4f67ebc412c
MatveyPoz <90983043+MatveyPoz@users.noreply.github.com>
2025-02-28 23:51:24 +0200

    added comments

---
+++ main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"encoding/json"
+	"net/http"
+	"time"
+)
+
+type TimeResponse struct {
+	Time string `json:"time"`
+}
+
+func timeHandler(w http.ResponseWriter, r *http.Request) {
+	response := TimeResponse{Time: time.Now().Format(time.RFC3339)}
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(response)
+}
+
+func main() {
+	// При зверенні на порт 8795 з endpoint /time, повертаємо нинішній час у вигляді YYYY-MM-DDTHH:MM:SSZ 
+	http.HandleFunc("/time", timeHandler)
+	http.ListenAndServe(":8795", nil)
+}
